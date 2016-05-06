19bf52007252203735ed71f910e393c958ea482d
Tomasz Lewandowski <lewandowski.tomasz93@yahoo.pl>
2016-05-06 23:50:59 +0200

    [Google, Gmail, Keywords] Keywords management
    1. Gmail keywords management
    2. Collecting keywords from email subjects

--- advservice.py
+++ advservice.py
@@ -6,6 +6,7 @@ from adv_utils import UserKeywordsDatabase
 
 app = flask.Flask(__name__)
 tkdb = UserKeywordsDatabase(os.environ['DATABASE_URL'], "adv_twitter_hashtags")
+gtkdb = UserKeywordsDatabase(os.environ['DATABASE_URL'], "adv_google_gmail")
 
 @app.route('/users/<userID>/twitter/hashtags', methods=['GET'])
 def getUserHashtags(userID):
@@ -29,6 +30,38 @@ def putUserHashtag(userID,keyword):
 @app.route('/users/<userID>/twitter/hashtags', methods=['DELETE'])
 def deleteUserHashtags(userID):
   tkdb.deleteKeywords(userID)
+  return ('',204)
+
+@app.route('/users/<user_id>/google/gmail/keywords', methods=['GET'])
+def getGmailKeywords(user_id):
+  keywords = gtkdb.getKeywords(user_id)
+  return json.dumps(keywords)
+
+@app.route('/users/<user_id>/google/gmail/subjects', methods=['POST'])
+def putGmailTitle(user_id):
+  content = flask.request.get_json(silent = True)
+  if content is None:
+    return ('',400)
+  if "subject" not in content:
+    return ('', 400)
+  keywords = content["subject"].split()
+  result = {}
+  result["keywords_updated"] = 0
+  for key in keywords:
+    try:
+      gtkdb.insertKeyword(user_id,key.lower())
+      result["keywords_updated"] += 1
+    except psycopg2.IntegrityError:
+      keyword = gtkdb.getKeyword(user_id,key.lower())
+      if keyword is None:
+        continue
+      rowcount = gtkdb.updateKeywordCount(user_id,keyword["keyword"],keyword["count"]+1)
+      result["keywords_updated"] += rowcount
+  return json.dumps(result)
+
+@app.route('/users/<user_id>/google/gmail/keywords', methods=['DELETE'])
+def deleteGmailKeywords(user_id):
+  gtkdb.deleteKeywords(user_id)
   return ('',204)
 
 if __name__ == '__main__':
